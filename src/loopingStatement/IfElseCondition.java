be10c42b8d8c671156457fd53962194e34b165c5
masud3386 <masud3386@yahoo.com>
2023-03-05 08:02:16 -0500

    submit my java program

---
+++ src/loopingStatement/IfElseCondition.java
@@ -0,0 +1,26 @@
+package loopingStatement;
+
+public class IfElseCondition {
+
+	public static void main(String[] args) {
+		
+		int day=3;
+		if(day==1)
+		{
+			System.out.println("I like java");
+		}
+		else if(day==2)
+		{
+			System.out.println("I don't like java");
+		}
+		else if(day==3)
+		{
+			System.out.println("you need more practic");
+		}else
+		{
+			System.out.println("Come to class everyday");
+		}
+
+	}
+
+}
